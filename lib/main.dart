63e0eadb0109b9d46b4280d6a43d6ed3ce56f95b
sabateymurov <sabirteymurov2@gmail.com>
2024-02-06 12:02:41 +0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:black_jack/screens/black_jack_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Black Jack',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: BlackJackScreen(),
+    );
+  }
+}
